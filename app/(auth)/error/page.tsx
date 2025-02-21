cda6a2a524766c6c4d0e252469e69dad60b94328
mactony <anthonymuthui6e4@gmail.com>
2025-02-21 09:53:54 +0300

    error page'

--- app/(auth)/error/page.tsx
+++ app/(auth)/error/page.tsx
@@ -1,9 +1,16 @@
 import { Suspense } from "react";
 import ErrorContent from "./ErrorContent";
+import { Loader2 } from "lucide-react";
 
 export default function AuthError() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="min-h-screen">
+          <Loader2 className="animate-spin bg-[#12362e]" />
+        </div>
+      }
+    >
       <ErrorContent />
     </Suspense>
   );
